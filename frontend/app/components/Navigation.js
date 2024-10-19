63c21502557041ac7298f5a340865381fdfe65d0
Serhii Prykhozhyi <sprihozhiy@gmail.com>
2024-10-19 10:25:41 -0600

    prepare the frontend for development

---
+++ frontend/app/components/Navigation.js
@@ -0,0 +1,13 @@
+import Image from "next/image"
+import Link from "next/link"
+import logo from '@/images/logo.svg'
+
+export default function Navigation() {
+  return (
+    <div className="p-3">
+        <Link href="/">
+            <Image src={logo} width={180} alt="logo" />
+        </Link>
+    </div>
+  )
+}
