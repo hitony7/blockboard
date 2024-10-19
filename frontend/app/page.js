aaef6cc3a2bc907536cce8808a331a61cd79814d
syrilj <83786796+syrilj@users.noreply.github.com>
2024-10-19 11:17:56 -0600

    footer

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import Navigation from "./components/Navigation";
-
+import Footer from "./components/Footer/Footer";
 export default function Home() {
   return (
     <div className="container mx-w-7xl">
       <Navigation />
       <main>
 
-      </main>
-      <footer>
 
-      </footer>
+      </main>
+      <Footer />
+     
     </div>
   );
 }
