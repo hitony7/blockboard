c3cc47ecfb6ebb6590203f0439c000f920929f8f
Serhii Prykhozhyi <sprihozhiy@gmail.com>
2024-10-19 10:25:41 -0600

    prepare the frontend for development

---
+++ frontend/app/utils/fonts.js
@@ -0,0 +1,4 @@
+import { Poppins, Roboto } from "next/font/google"
+
+export const headingFont = Poppins({subsets: ["latin"], weight: ["400", "600", "700"]});
+export const bodyFont = Roboto({subsets: ["latin"], weight: ["300", "400"]});
