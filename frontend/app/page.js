2f26174879e38529331c3385ba0391769cdd76b9
syrilj <83786796+syrilj@users.noreply.github.com>
2024-10-19 15:25:27 -0600

    buyer page setup

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer/Footer";
-import VotingPage from "./pages/VotingPage";
+
 export default function Home() {
   return (
     <div className="container mx-w-7xl">
@@ -9,7 +9,7 @@ export default function Home() {
       <main>
       </main>
       <Footer />
-     
+
     </div>
   );
 }
